3a57c8705f74f90d918769eb44de7e0081c47da7
Alejandro Agudelo Parrado <Alejandro.Agudelo@bizagi.com>
2019-06-05 07:06:39 -0500

    Add UnaQuincena payment method

---
+++ src/Plugins/Nop.Plugin.Payments.UnaQuincena/Models/ConfigurationModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Payments.UnaQuincena.Models
+{
+    public class ConfigurationModel : BaseNopModel, ILocalizedModel<ConfigurationModel.ConfigurationLocalizedModel>
+    {
+        public ConfigurationModel()
+        {
+            Locales = new List<ConfigurationLocalizedModel>();
+        }
+
+        public int ActiveStoreScopeConfiguration { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payment.CheckMoneyOrder.DescriptionText")]
+        public string DescriptionText { get; set; }
+        public bool DescriptionText_OverrideForStore { get; set; }
+        public IList<ConfigurationLocalizedModel> Locales { get; set; }
+
+        #region Nested class
+
+        public partial class ConfigurationLocalizedModel : ILocalizedLocaleModel
+        {
+            public int LanguageId { get; set; }
+
+            [NopResourceDisplayName("Plugins.Payment.CheckMoneyOrder.DescriptionText")]
+            public string DescriptionText { get; set; }
+        }
+
+        #endregion
+    }
+}
